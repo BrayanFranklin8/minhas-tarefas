baf40f4965df9d868279c6977fd276896b466050
BrayanFranklin8 <brayanfranklin88@gmail.com>
2025-09-29 05:43:17 -0300

    finalizando edit e remover

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,43 +1,34 @@
-import Tarefa from '../../components/Tarefa'
+import { useSelector } from 'react-redux'
+
+import Tarefa from '../components/Tarefa'
 import { Container } from './styles'
 
-const tarefas = [
-  {
-    titulo: 'Estudar',
-    descricao: 'ver a aula 3',
-    prioridade: 'importante',
-    status: 'pendente'
-  },
-  {
-    titulo: 'Pagar a internent',
-    descricao: 'baixar a fatura',
-    prioridade: 'urgente',
-    status: 'concluida'
-  },
-  {
-    titulo: 'Ir a academia',
-    descricao: 'fazer o treino B',
-    prioridade: 'importante',
-    status: 'pendente'
-  }
-]
+import { RootReducer } from '../../store'
 
-const ListaDeTarefas = () => (
-  <Container>
-    <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
-    <ul>
-      {tarefas.map((t) => (
-        <li key={t.titulo}>
-          <Tarefa
-            descricao={t.descricao}
-            titulo={t.titulo}
-            prioridade={t.prioridade}
-            status={t.status}
-          />
-        </li>
-      ))}
-    </ul>
-  </Container>
-)
+const ListaDeTarefas = () => {
+  const tarefas = useSelector((state: RootReducer) => state.tarefas.itens)
+
+  return (
+    <Container>
+      <p>
+        {tarefas.length} tarefas marcadas como: &quot;categoria&ldquo; e
+        &quot;termo&ldquo;
+      </p>
+      <ul>
+        {tarefas.map((t) => (
+          <li key={t.id}>
+            <Tarefa
+              id={t.id}
+              descricao={t.descricao}
+              titulo={t.titulo}
+              prioridade={t.prioridade}
+              status={t.status}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
 
 export default ListaDeTarefas
--- src/containers/components/FiltroCard/styles.ts
+++ src/containers/components/FiltroCard/styles.ts
@@ -1,21 +1,23 @@
 import styled from 'styled-components'
 
-import { Props } from './index'
+import { Props } from '.'
 
 type PropsSemLegendaEContador = Omit<Props, 'contador' | 'legenda'>
 
 export const Card = styled.div<PropsSemLegendaEContador>`
   padding: 8px;
-  border: 1px solid ${(props) => (props.ativo ? '#1e90ff' : '#a1a1a1')};
+  border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#a1a1a1')};
   background-color: ${(props) => (props.ativo ? '#fff' : '#fcfcfc')};
-  color: ${(props) => (props.ativo ? '#1e90ff' : '#5e5e5e')};
+  color: ${(props) => (props.ativo ? '#1E90FF' : '#5e5e5e')};
   border-radius: 8px;
 `
+
 export const Contador = styled.span`
   font-weight: bold;
-  font-seize: 24px;
+  font-size: 24px;
   display: block;
 `
+
 export const Label = styled.span`
   font-size: 14px;
 `
